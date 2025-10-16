4b9e5265df9bfc77f1c306b211a95970a3df939f
Abhishek <abhi84514aaa.in@gmail.com>
2025-10-16 14:40:12 +0530

    creating different routes

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -18,6 +18,27 @@ func main() {
 	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from teacher route"))
 		fmt.Println("Hello teacher Route")
+		fmt.Println(r.Method)
+
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Hello get method from teachers route"))
+			fmt.Println("Hello method get on teachers routes")
+		case http.MethodPatch:
+			w.Write([]byte("Hello patch method from teachers route"))
+			fmt.Println("Hello method patch on teachers routes")
+		case http.MethodPost:
+			w.Write([]byte("Hello post method from teachers route"))
+			fmt.Println("Hello method post on teachers routes")
+		case http.MethodPut:
+			w.Write([]byte("Hello put method from teachers route"))
+			fmt.Println("Hello method put on teachers routes")
+		case http.MethodDelete:
+			w.Write([]byte("Hello delete method from teachers route"))
+			fmt.Println("Hello method delete on teachers routes")
+		default:
+			w.Write([]byte("Hello from nothing method haahah"))
+		}
 	})
 
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
